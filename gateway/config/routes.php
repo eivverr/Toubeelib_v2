c88481a2c4165c388fa60b5e46c28b1e967c9f2e
Weier Loris <lorisweier@gmx.fr>
2025-01-08 16:17:18 +0100

    Update routes.php

--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -1,1 +1,10 @@
 <?php
+
+use Slim\App;
+
+return function(App $app): App {
+
+    $app->get('/praticiens/{id}', \toubeelib\gateway\application\actions\ConsulterPraticienAction::class);
+
+    return $app;
+};
