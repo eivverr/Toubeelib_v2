5650787bed59170fed8b6f5564e2c65f12cda9f6
Weier Loris <lorisweier@gmx.fr>
2025-01-08 16:14:49 +0100

    Update ConsulterPraticienAction.php

---
+++ gateway/src/application/actions/ConsulterPraticienAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace toubeelib\gateway\application\actions;
+
+use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ConsulterPraticienAction extends AbstractGatewayAction
+{
+
+    private ClientInterface $remote_praticien_service;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->remote_praticien_service = $client;
+    }
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+        $id = $args['id'];
+        $response = $this->remote_praticien_service->get("/praticiens/$id");
+
+        return $response;
+    }
+}
