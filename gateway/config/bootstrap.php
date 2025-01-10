e19521630442d875b088edc3a0bcd017e153ae31
Okiles <colsonlenny5@gmail.com>
2025-01-10 14:33:29 +0100

    Updating routes

--- gateway/config/bootstrap.php
+++ gateway/config/bootstrap.php
@@ -5,10 +5,20 @@ use Slim\Factory\AppFactory;
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions(__DIR__ . '/settings.php' );
-//$builder->addDefinitions(__DIR__ . '/dependencies.php');
+$builder->addDefinitions(__DIR__ . '/dependencies.php');
 
 $c = $builder->build();
 $app = AppFactory::createFromContainer($c);
+/*
+$app->add(new CorsMiddleware());
+$app->options('/{routes:.+}',
+    function(Request $rq, Response $rs, array $args) : Response {
+        return $rs;
+    });*/
+
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false);
 
 $app = (require_once __DIR__ . '/routes.php')($app);
 $routeParser = $app->getRouteCollector()->getRouteParser();
