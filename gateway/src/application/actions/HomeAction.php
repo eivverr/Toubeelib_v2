3471913a679d8cdb8a7b3d6b1a3f13a5e6dd3c87
Weier Loris <lorisweier@gmx.fr>
2025-01-10 15:41:27 +0100

    index + home fix

--- gateway/src/application/actions/HomeAction.php
+++ gateway/src/application/actions/HomeAction.php
@@ -2,15 +2,22 @@
 
 namespace toubeelib\gateway\application\actions;
 
+use GuzzleHttp\ClientInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use toubeelib\gateway\application\actions\AbstractGatewayAction;
 
 class HomeAction extends AbstractGatewayAction
 {
+    private ClientInterface $remote_home_service;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->remote_home_service = $client;
+    }
+
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
-        $rs->getBody()->write("Hello, world!");
-        return $rs;
+        return $this->remote_home_service->get("/");
     }
 }
