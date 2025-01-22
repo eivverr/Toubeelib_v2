000e799db1b9f1f81f67afb8dbf8bd89ec37fa38
Weier Loris <lorisweier@gmx.fr>
2025-01-22 15:25:01 +0100

    add require files

---
+++ app-rdvs/src/application/middlewares/authz/AuthzPraticienMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace toubeelib\application\middlewares\authz;
+
+use toubeelib\core\services\authorization\AuthzPraticienServiceInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class AuthzPraticienMiddleware
+{
+    private AuthzPraticienServiceInterface $authzPraticienService;
+
+    public function __construct(AuthzPraticienServiceInterface $authzPraticienService)
+    {
+        $this->authzPraticienService = $authzPraticienService;
+    }
+
+    public function __invoke() : Response
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
---
+++ app-rdvs/src/core/dto/AuthDTO.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace toubeelib\core\dto;
+
+use toubeelib\core\domain\entities\User;
+use toubeelib\core\dto\DTO;
+
+class AuthDTO extends DTO
+{
+    protected string $id;
+    protected string $email;
+    protected int $role;
+    protected string $token;
+    protected string $refreshToken;
+
+    public function __construct(User $user)
+    {
+        $this->id = $user->getId();
+        $this->email = $user->getEmail();
+        $this->role = $user->getRole();
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getRole(): int
+    {
+        return $this->role;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    public function getRefreshToken(): string
+    {
+        return $this->refreshToken;
+    }
+
+    public function setToken(string $token): void
+    {
+        $this->token = $token;
+    }
+
+    public function setRefreshToken(string $refreshToken): void
+    {
+        $this->refreshToken = $refreshToken;
+    }
+}
