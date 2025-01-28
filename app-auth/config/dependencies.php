a0b87730bf6be676795e2464a7f56efffce17991
Okiles <colsonlenny5@gmail.com>
2025-01-28 14:19:17 +0100

    Update dependencies.php

--- app-auth/config/dependencies.php
+++ app-auth/config/dependencies.php
@@ -8,4 +8,15 @@ return [
     //      Actions
     // #####################
 
+    'PostSigninAction' => function(ContainerInterface $container) {
+        return new \toubeelib\app\auth\application\actions\PostSigninAction($container);
+    },
+
+    'PostRegisterAction' => function(ContainerInterface $container) {
+        return new \toubeelib\app\auth\application\actions\PostRegisterAction($container);
+    },
+
+    'PostRefreshAction' => function(ContainerInterface $container) {
+        return new \toubeelib\app\auth\application\actions\PostRefreshAction($container);
+    },
 ];
